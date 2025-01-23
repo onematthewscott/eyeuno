8dd41fbe550121a790b64ee1694b89d3f1b2de42
onematthewscott <herna757@msu.edu>
2025-01-23 09:43:09 +0200

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '50px' }}>
+      <h1>EYEUNO</h1>
+      <p>The Matthew Scott Collection</p>
+    </div>
+  );
+}
+
+export default App;
